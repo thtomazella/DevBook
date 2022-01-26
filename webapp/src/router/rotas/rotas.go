e4ec911b96582636cf72224409651abde2798e07
thtomazella <th.tomazella@gmail.com>
2022-01-25 21:58:17 -0300

    Tela Login

--- webapp/src/router/rotas/rotas.go
+++ webapp/src/router/rotas/rotas.go
@@ -17,9 +17,13 @@ type Rota struct {
 // Configurar coloca todas as rotas dentro do Router
 func Configurar(router *mux.Router) *mux.Router {
 	rotas := rotasLogin
-	
+
 	for _, rota := range rotas {
 		router.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
 	}
+
+	fileServer := http.FileServer(http.Dir("./assets/"))
+	router.PathPrefix("/assets").Handler(http.StripPrefix("/assets/", fileServer))
+
 	return router
 }
