5be442b3f1437fb3e2963e90838cb3cfc42ca1f2
thtomazella <th.tomazella@gmail.com>
2022-01-30 10:04:09 -0300

    Requisicao com autenticacao

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"webapp/src/config"
+	"webapp/src/requisicoes"
 	"webapp/src/utils"
 )
 
@@ -17,5 +20,8 @@ func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
 
 // CarregarPaginaPrincipal vai carregar a pagina principal com as publicações
 func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
+	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodGet, url, nil)
+	fmt.Println(response.StatusCode, erro)
 	utils.ExecutarTemplate(w, "home.html", nil)
 }
