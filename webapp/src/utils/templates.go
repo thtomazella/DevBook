9bfee9ab8801fc421469e53be78034b1d46bd8d7
thtomazella <th.tomazella@gmail.com>
2022-01-31 21:04:38 -0300

    Barra de Navegaçao e templates

--- webapp/src/utils/templates.go
+++ webapp/src/utils/templates.go
@@ -10,6 +10,7 @@ var templates *template.Template
 // CarregarTemplates insere os templates hmtl na variável templates
 func CarregarTemplates() {
 	templates = template.Must(template.ParseGlob("views/*.html"))
+	templates = template.Must(templates.ParseGlob("views/templates/*.html"))
 }
 
 // ExecutarTemplate renderiza uma página html na tela
